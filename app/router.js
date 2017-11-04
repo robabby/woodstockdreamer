0dccdd1e99bd8e9126f4a4376cc4a9eb54f0cc51
robabby <robabby23@gmail.com>
2017-11-04 14:58:11 -0500

    Created initial routes and connected to Wordpress

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('posts', function() {
+    this.route('post', { path: ':post_slug' });
+  });
 });
 
 export default Router;
--- config/environment.js
+++ config/environment.js
@@ -7,6 +7,7 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+    wordpressHost: 'http://woodstockdreamer.com/wp/',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
