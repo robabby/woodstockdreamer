0b530228f736f8018615d982cdba2bc551d42b42
robabby <robabby23@gmail.com>
2017-11-05 10:19:05 -0600

    Added new wp-host

--- config/environment.js
+++ config/environment.js
@@ -7,30 +7,7 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
-    // contentSecurityPolicy: {
-    //   // Deny everything by default
-    //   'default-src': "'none'",
-    //
-    //   // Allow scripts at https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js
-    //   'script-src': ["'self'"],
-    //
-    //   // Allow fonts to be loaded from http://fonts.gstatic.com
-    //   'font-src': ["'self'", "http://fonts.gstatic.com"],
-    //
-    //   // Allow data (xhr/websocket) from api.mixpanel.com and custom-api.local
-    //   'connect-src': ["'self'", "https:", "http://localhost:3000", "http://localhost:3000/members", "http://woodstockdreamer.com/wp/wp-json/wp/v2/posts"],
-    //
-    //   // Allow images from the origin itself (i.e. current domain)
-    //   'img-src': ["'self'", "data:"],
-    //
-    //   // Allow CSS loaded from https://fonts.googleapis.com
-    //   'style-src': ["'self'", "https://fonts.googleapis.com"],
-    //
-    //   // Omit `media-src` from policy
-    //   // Browser will fallback to default-src for media resources (which is 'none', see above)
-    //   'media-src': null
-    // },
-    wordpressHost: 'http://woodstockdreamer.com/wp',
+    wordpressHost: 'https://woodstockdreamer-wp.herokuapp.com/',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
