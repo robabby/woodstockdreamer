1a9141109ee2dee790a1311cca34eaf5404c55f1
robabby <robabby23@gmail.com>
2017-11-05 01:32:10 -0500

    Added toast

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,15 +5,21 @@ export default Controller.extend({
     firstName: 'Tony',
     lastName: 'Stark',
     email: 'tony@avengers.com',
+    showMessage: false,
     actions: {
       addSubscriber() {
+        let self = this;
         let person = this.get('store').createRecord('member', {
           firstName: this.get('firstName'),
           lastName: this.get('lastName'),
           email: this.get('email')
         });
 
-        person.save();
+        person.save().then(() => {
+          self.set('showMessage', true)
+        }).catch((err) => {
+          Ember.Logger.info(err);
+        });;
       }
     }
 });
