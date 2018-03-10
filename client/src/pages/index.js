d59f903ec310e542dfac401442ff65287c8a77a1
robabby <robabby23@gmail.com>
2018-03-10 16:12:02 -0600

    removed local strapi

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,35 +3,117 @@ import Link from 'gatsby-link'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
 
+import Header from '../components/Header'
+import Main from '../components/Main'
+import Footer from '../components/Footer'
+
 import pic01 from '../images/pic01.jpg'
 
-class BlogIndex extends React.Component {
+class Index extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isArticleVisible: false,
+      timeout: false,
+      articleTimeout: false,
+      article: '',
+      loading: 'is-loading'
+    }
+    this.handleOpenArticle = this.handleOpenArticle.bind(this)
+    this.handleCloseArticle = this.handleCloseArticle.bind(this)
+  }
+
+  componentDidMount () {
+    console.log(this.state.article);
+    this.timeoutId = setTimeout(() => {
+        this.setState({loading: ''});
+    }, 100);
+  }
+
+  componentWillUnmount () {
+    if (this.timeoutId) {
+        clearTimeout(this.timeoutId);
+    }
+  }
+
+  handleOpenArticle(article) {
+
+    this.setState({
+      isArticleVisible: !this.state.isArticleVisible,
+      article
+    })
+
+    setTimeout(() => {
+      this.setState({
+        timeout: !this.state.timeout
+      })
+    }, 325)
+
+    setTimeout(() => {
+      this.setState({
+        articleTimeout: !this.state.articleTimeout
+      })
+    }, 350)
+
+  }
+
+  handleCloseArticle() {
+
+    this.setState({
+      articleTimeout: !this.state.articleTimeout
+    })
+
+    setTimeout(() => {
+      this.setState({
+        timeout: !this.state.timeout
+      })
+    }, 325)
+
+    setTimeout(() => {
+      this.setState({
+        isArticleVisible: !this.state.isArticleVisible,
+        article: ''
+      })
+    }, 350)
+
+  }
+
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allMarkdownRemark.edges')
+    const articles = this.props.data.allStrapiArticle.edges
+    console.log("/articles/", articles);
 
     return (
-      <div>
-        <Helmet>
-          <title>{get(this, 'props.data.site.siteMetadata.title')}</title>
-          <meta name="description" content={get(this, 'props.data.site.siteMetadata.description')} />
-        </Helmet>
+      <div className={`body ${this.state.loading} ${this.state.isArticleVisible ? 'is-article-visible' : ''}`}>
+
+        <Header onOpenArticle={this.handleOpenArticle} timeout={this.state.timeout} />
+        <Main
+          isArticleVisible={this.state.isArticleVisible}
+          timeout={this.state.timeout}
+          articleTimeout={this.state.articleTimeout}
+          article={this.state.article}
+          articles={articles}
+          onCloseArticle={this.handleCloseArticle}
+        />
+        <Footer timeout={this.state.timeout} />
+
+        <div id="bg"></div>
       </div>
     )
   }
 }
 
-BlogIndex.propTypes = {
+Index.propTypes = {
   route: React.PropTypes.object,
 }
 
-export default BlogIndex
+export default Index
 
 export const pageQuery = graphql`
-  query IndexQuery {
+  query PageQuery {
     site {
       siteMetadata {
         title
+        description
       }
     }
   }
